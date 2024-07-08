d03978720d5e6120322fe063f550d687fe01c0c5
hazem-alsaqaan <hazem.alsaqaan@gmail.com>
2024-07-08 12:01:42 +0300

    isolate a dummy data in another file

---
+++ src/dummyData.ts
@@ -0,0 +1,44 @@
+interface TdummyData {
+    id: number;
+    title: string;
+    picture: string;
+    description: string;
+    price: number;
+    quantity: number
+}
+export const dummyDataApp : TdummyData[] = [
+    {
+      id: 1,
+      title: "Blender",
+      picture:
+        "https://img.freepik.com/free-photo/blender-juice-machine_1203-7838.jpg?t=st=1720218844~exp=1720222444~hmac=98d1420df1ddcc775b5f9da8201f3a9b8528acfe102ccedbab52394ec5ce4918&w=826",
+      description: "an electric culinary grinding and mixing appliance",
+      price: 65,
+      quantity: 1,
+    },
+    {
+      id: 2,
+      title: "screen",
+      picture:
+        "https://img.freepik.com/free-psd/helmet-mock-up_1310-157.jpg?w=826&t=st=1720218999~exp=1720219599~hmac=c7739e47adc0ed865576f817a32cfb72abdb3f21354e5bf45477770467fad0bb",
+      description: "Samsung 85 Inch TV Crystal Processor 4K LED",
+      price: 1200,
+      quantity: 1,
+    },
+    {
+      id: 3,
+      title: "screen",
+      picture: "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
+      description: "Tornado HD Smart TV 32 Inch, Built-In Receiver 32ES9300E",
+      price: 3200,
+      quantity: 1,
+    },
+    {
+      id: 4,
+      title: "screen",
+      picture: "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg",
+      description: "LG, UHD 4K TV, 65 inch UR78 series, WebOS Smart AI ThinQ",
+      price: 4200,
+      quantity: 1,
+    },
+  ];
